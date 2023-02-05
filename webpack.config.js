003c2b717960869671b1690d888c3761fb54dcfb
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-02-05 12:15:04 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
